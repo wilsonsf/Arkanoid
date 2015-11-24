00e05068f672582e40fcffef21a867460de54d48
Wilson de Farias <wilsonsf1@gmail.com>
2015-11-24 09:43:32 -0200

    Passo 2: Criado objeto bola e renderizando ele na tela

--- main.cpp
+++ main.cpp
@@ -6,15 +6,30 @@
  */
 
 using namespace std;
-using namespace sf;
+// using namespace sf;
 
 // 'constexpr' define um valor imutável em tempo de compilação
 constexpr int windowWidth{800}, windowHeight{600};
+constexpr float ballRadius{10.f};
+
+class Ball {
+public:
+	sf::CircleShape shape;
+
+	Ball(float mX, float mY) {
+		shape.setPosition(mX, mY);
+		shape.setRadius(ballRadius);
+		shape.setFillColor(sf::Color::Red);
+		shape.setOrigin(ballRadius, ballRadius);
+	}
+};
 
 int main () {
+	Ball ball{windowWidth / 2, windowHeight /2 };
+
 
 	// RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Arkanoid -1");
-	RenderWindow window({windowWidth,windowHeight}, "Arkanoid - 1");
+	sf::RenderWindow window({windowWidth,windowHeight}, "Arkanoid - 1");
 	window.setFramerateLimit(60);
 
 	// Loop do jogo
@@ -23,13 +38,17 @@ int main () {
 		// Monitora os eventos e reage de acordo
 		sf::Event event;
 		while(window.pollEvent(event)) {
-			if(event.type == sf::Event::KeyPressed && event.key.code == Keyboard::Key::Escape) {
+			if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Key::Escape) {
 				window.close();
 			}
 		}
 
 		// Limpa a tela 
-		window.clear(Color::Black);
+		window.clear(sf::Color::Black);
+
+		// Renderiza os objetos na tela
+		window.draw(ball.shape);
+
 
 		// Exibe os conteúdos da tela
 		window.display();
